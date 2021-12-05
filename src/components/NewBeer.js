da95376dff2c2e2cf3e3cb8fc172025c77cb5dfd
Maik Jüngling <juengling.maik@gmail.com>
2021-12-06 00:42:38 +0100

    6done

--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -1,5 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function NewBeer() {
-  return <div>NewBeerPage</div>;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let newBeer = {
+      name: event.target.name.value,
+      tagline: event.target.tagline.value,
+      description: event.target.description.value,
+      first_brewed: event.target.first_brewed.value,
+      brewers_tips: event.target.brewers_tips.value,
+      attenuation_level: event.target.attenuation_level.value,
+      contributed_by: event.target.contributed_by.value,
+    };
+    await axios.post('https://ih-beers-api2.herokuapp.com/beers/new', newBeer);
+  };
+  return (
+    <div>
+      <h1>Add new Beer</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="name" type="text" placeholder="Enter name" />
+        <input name="tagline" type="text" placeholder="Enter tagline" />
+        <input name="description" type="text" placeholder="Enter description" />
+        <input
+          name="first_brewed"
+          type="text"
+          placeholder="Enter first brewed"
+        />
+        <input
+          name="brewers_tips"
+          type="text"
+          placeholder="Enter brewers tips"
+        />
+        <input
+          name="attenuation_level"
+          type="number"
+          placeholder="Enter attenuation level"
+        />
+        <input
+          name="contributed_by"
+          type="text"
+          placeholder="Enter contributer"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
 import axios from 'axios';
 
 export default function BeerDetail() {
-  const { id } = useParams();
   const [beerDetail, setDetail] = useState(null);
 
   useEffect(() => {
     (async () => {
-      let { data } = await axios.get(
-        `https://ih-beers-api2.herokuapp.com/beers/random`
-      );
+      let {
+        data: {
+          name,
+          image_url,
+          tagline,
+          first_brewed,
+          attenuation_level,
+          description,
+          contributed_by,
+        },
+      } = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`);
 
       let beer = {
-        id: id,
-        name: data.name,
-        image: data.image_url,
-        tagline: data.tagline,
-        firstBrew: data.first_brewed,
-        attLvl: data.attenuation_level,
-        desc: data.description,
-        contributed: data.contributed_by,
+        name,
+        image_url,
+        tagline,
+        first_brewed,
+        attenuation_level,
+        description,
+        contributed_by,
       };
       setDetail(beer);
     })();
@@ -32,13 +37,13 @@ export default function BeerDetail() {
 
   return (
     <div>
-      <img src={beerDetail.image} />
+      <img src={beerDetail.image_url} alt="" />
       <h1 className="font-bold">{beerDetail.name}</h1>
-      <label>{beerDetail.attLvl}</label>
+      <label>{beerDetail.attenuation_level}</label>
       <h2>{beerDetail.tagline}</h2>
-      <label>{beerDetail.firstBrew}</label>
-      <p>{beerDetail.desc}</p>
-      <label>{beerDetail.contributed}</label>
+      <label>{beerDetail.first_brewed}</label>
+      <p>{beerDetail.description}</p>
+      <label>{beerDetail.contributed_by}</label>
     </div>
   );
 }
